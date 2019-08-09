95a76fa5b0ae8554a169cf0d47703dd8ae2363cd
Dorell James <galangdj@gmail.com>
2019-08-09 10:55:47 +0800

    Updating heroku-create-app.js

--- heroku-create-app.js
+++ heroku-create-app.js
@@ -25,6 +25,8 @@ app.post('/', function(req, res) {
     data: { name }
   })
     .then(function (response) {
+        
+      
       res.status(201).json(response.data);
     })
     .catch(function (error) {
