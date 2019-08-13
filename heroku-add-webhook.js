5c322425e0f576a8e15fc583688990a5033e089c
Dorell James <galangdj@gmail.com>
2019-08-13 15:23:07 +0800

    Create heroku-add-webhook.js

---
+++ heroku-add-webhook.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var Webtask = require('webtask-tools');
+var bodyParser = require('body-parser');
+var axios = require('axios');
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/', function(req, res) {
+  const { APP_TOKEN } = req.webtaskContext.secrets;
+  const { app_id } = req.body;
+  if (!app_id) {
+    return res
+      .status(400)
+      .json({ name: 'App ID is required to set webhooks!' });
+  }
+
+  axios({
+    url: `https://api.heroku.com/apps/${app_id}/webhooks`,
+    method: 'POST',
+    headers: {
+      Accept: 'application/vnd.heroku+json; version=3',
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + APP_TOKEN,
+    },
+    data: {
+      level: 'sync',
+      name: 'notify-webriq-app-for-release',
+      include: ['api:release'],
+      url: 'https://app.webriq.com/hooks',
+    },
+  })
+    .then(function(response) {
+      res.status(201).json(response.data);
+    })
+    .catch(function(error) {
+      res.status(500).json(error);
+    });
+});
+
+module.exports = Webtask.fromExpress(app);
