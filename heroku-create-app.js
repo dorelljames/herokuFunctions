f8b23c3c1ce7af61bf4cd5282277383ee436111d
Dorell James <galangdj@gmail.com>
2019-08-19 17:13:57 +0800

    Updating heroku-create-app.js

--- heroku-create-app.js
+++ heroku-create-app.js
@@ -7,7 +7,7 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/', function(req, res) {
-  const { APP_TOKEN } = req.webtaskContext.secrets;
+  const { APP_TOKEN_LIVE } = req.webtaskContext.secrets;
 
   const { name } = req.body;
   if (!name) {
