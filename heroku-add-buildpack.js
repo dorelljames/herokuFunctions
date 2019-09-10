6b74259ed3bdb3f7064e290a833e38ac73ba3450
Dorell James <galangdj@gmail.com>
2019-09-10 15:19:43 +0800

    Updating heroku-add-buildpack.js

--- heroku-add-buildpack.js
+++ heroku-add-buildpack.js
@@ -6,8 +6,7 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/', function(req, res) {
-  const { APP_TOKEN_LIVE } = req.webtaskContext.secrets;
+function addBuildpacks({ req, res, APP_TOKEN }) {
   const { app_id, buildpack } = req.body;
   if (!app_id) {
     return res
@@ -15,24 +14,32 @@ app.post('/', function(req, res) {
       .json({ name: 'App ID is required to enable addon!' });
   }
 
-  axios({
+  return axios({
     url: `https://api.heroku.com/apps/${app_id}/buildpack-installations`,
     method: 'PUT',
     headers: {
       Accept: 'application/vnd.heroku+json; version=3',
       'Content-Type': 'application/json',
-      Authorization: 'Bearer ' + APP_TOKEN_LIVE,
+      Authorization: 'Bearer ' + APP_TOKEN,
     },
     data: {
       updates: buildpack,
     },
   })
-    .then(function(response) {
-      res.status(200).json(response.data);
-    })
-    .catch(function(error) {
-      res.status(500).json(error);
-    });
+    .then(response => res.status(200).json(response.data))
+    .catch(err => res.status(500).json(err));
+}
+
+app.post('/', async function(req, res) {
+  const { APP_TOKEN } = req.webtaskContext.secrets;
+
+  await addBuildpacks({ req, res, APP_TOKEN });
+});
+
+app.post('/LIVE', async function(req, res) {
+  const { APP_TOKEN_LIVE: APP_TOKEN } = req.webtaskContext.secrets;
+
+  await addBuildpacks({ req, res, APP_TOKEN });
 });
 
 module.exports = Webtask.fromExpress(app);
