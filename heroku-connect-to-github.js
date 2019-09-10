e596383bfc8490d18aa2059df9d3fb2dd0e2f63b
Dorell James <galangdj@gmail.com>
2019-09-10 15:29:52 +0800

    Updating heroku-connect-to-github.js

--- heroku-connect-to-github.js
+++ heroku-connect-to-github.js
@@ -1,35 +1,43 @@
-var express    = require('express');
-var Webtask    = require('webtask-tools');
+var express = require('express');
+var Webtask = require('webtask-tools');
 var bodyParser = require('body-parser');
-var axios      = require("axios");
+var axios = require('axios');
 var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/', function(req, res) {
-  const { APP_TOKEN_LIVE } = req.webtaskContext.secrets;
-
+function connectToGitHub({ req, res, APP_TOKEN }) {
   const { repo_path, app_id } = req.body;
   if (!repo_path || !app_id) {
-    return res.status(400).json({ name: "App ID and repo owner with name is required!" });
+    return res
+      .status(400)
+      .json({ name: 'App ID and repo owner with name is required!' });
   }
 
-  axios({
+  return axios({
     url: `https://kolkrabbi.heroku.com/apps/${app_id}/github`,
     method: 'POST',
     headers: {
-      'Accept': 'application/vnd.heroku+json; version=3',
+      Accept: 'application/vnd.heroku+json; version=3',
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + APP_TOKEN_LIVE
+      Authorization: 'Bearer ' + APP_TOKEN,
     },
-    data: { repo: repo_path }
+    data: { repo: repo_path },
   })
-    .then(function (response) {
-      res.status(201).json(response.data);
-    })
-    .catch(function (error) {
-      res.status(500).json(error);
-    });
+    .then(response => res.status(201).json(response.data))
+    .catch(err => res.status(500).json(err));
+}
+
+app.post('/', async function(req, res) {
+  const { APP_TOKEN } = req.webtaskContext.secrets;
+
+  await connectToGitHub({ req, res, APP_TOKEN });
+});
+
+app.post('/LIVE', async function(req, res) {
+  const { APP_TOKEN_LIVE: APP_TOKEN } = req.webtaskContext.secrets;
+
+  await connectToGitHub({ req, res, APP_TOKEN });
 });
 
 module.exports = Webtask.fromExpress(app);
